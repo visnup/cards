c2eb500f0647780463c81168200901cf36135b9a
Kyle Kemp <kyle@seiyria.com>
2024-07-10 07:30:33 -0500

    add avif compression for images too

--- scripts/compress-card-images.ts
+++ scripts/compress-card-images.ts
@@ -1,6 +1,5 @@
 const fs = require('fs');
-const imagemin = require('imagemin');
-const webp = require('imagemin-webp');
+const sharp = require('sharp');
 
 const compressImages = async () => {
   const allDirs = fs.readdirSync('./dist/cards');
@@ -9,18 +8,33 @@ const compressImages = async () => {
     .map((dir) => {
       const allLocales = fs.readdirSync(`./dist/cards/${dir}`);
 
-      return allLocales.map((loc) =>
-        imagemin([`./dist/cards/${dir}/${loc}/*.png`], {
-          destination: `./dist/cards/${dir}/${loc}/`,
-          plugins: [
-            webp({
+      return allLocales.map((loc) => {
+        const allLocaleFiles = fs
+          .readdirSync(`./dist/cards/${dir}/${loc}`)
+          .filter((f) => f.includes('.png'));
+
+        return allLocaleFiles.map((file) => [
+          sharp(`./dist/cards/${dir}/${loc}/${file}`)
+            .avif({
+              quality: 20,
               lossless: true,
-            }),
-          ],
-        }),
-      );
+            })
+            .toFile(
+              `./dist/cards/${dir}/${loc}/${file.replace('.png', '.avif')}`,
+            ),
+
+          sharp(`./dist/cards/${dir}/${loc}/${file}`)
+            .webp({
+              quality: 20,
+              lossless: true,
+            })
+            .toFile(
+              `./dist/cards/${dir}/${loc}/${file.replace('.png', '.webp')}`,
+            ),
+        ]);
+      });
     })
-    .flat();
+    .flat(Infinity);
 
   await Promise.all(allPromises);
   console.log('Compressed images!');
